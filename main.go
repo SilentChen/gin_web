92209bba160fffa6407e3f7740ebea5f5bd905c2
SilentChen <609451243@qq.com>
2019-03-10 19:18:07 +0800

    log print fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gin"
 	"html/template"
 	"io"
@@ -36,25 +35,6 @@ func main() {
 	r.SetFuncMap(template.FuncMap{
 
 	})
-
-	// setting the logger format and use gin logger module
-	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-
-		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
-			param.ClientIP,
-			param.TimeStamp.Format("2006-01-02 15:04:05"),
-			param.Method,
-			param.Path,
-			param.Request.Proto,
-			param.StatusCode,
-			param.Latency,
-			param.Request.UserAgent(),
-			param.ErrorMessage,
-		)
-	}))
-
-	// use gin recovery module
-	r.Use(gin.Recovery())
 
 	r.LoadHTMLGlob("views/**/*")
 
--- router.go
+++ router.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"gin"
 	"net/http/pprof"
+	_"net/http/pprof"
 	"web/controllers/admin"
 	"web/controllers/api"
 )
 
-func ginPprof(router *gin.Engine) {
+func profRouter(router *gin.Engine) {
 
 	router.GET("/debug/pprof", func(c *gin.Context) {
 		pprof.Index(c.Writer, c.Request)
@@ -46,15 +48,32 @@ func LoadRouters() *gin.Engine {
 
 	r := gin.New()
 
+	// setting the logger format and use gin logger module
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format("2006-01-02 15:04:05"),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	// use gin recovery module
+	r.Use(gin.Recovery())
+
 	// some handler init
 	apiTest := new(api.Test)
 	adminIndex := new(admin.Index)
 
 	// link the route pattern to the handler
-
 	r.GET("/api", apiTest.Index)
 	r.GET("/admin", adminIndex.Index)
-	ginPprof(r)
+	profRouter(r)
 	/*articles := new(app.Articles)
 
 	r.GET("/articles", articles.Index)
